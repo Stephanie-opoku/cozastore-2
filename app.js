89ca80f2c3302e7605b31a054a69a7d2c3467a30
Stephanie-opoku <opokuafriyiestephanie@gmail.com>
2024-01-21 23:52:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3505;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+
+app.get('/home-02', (req, res) =>{
+    res.render('home-02');
+})
+
+app.get('/home-03', (req, res) =>{
+    res.render('home-03');
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+})
+
+app.get('/contact', (req, res)=>{
+    res.render('contact')
+})
+
+app.get('/blog', (req, res)=>{
+    res.render('blog')
+})
+
+app.listen(port,()=>{
+    console.log(`listening on port http://localhost:${port}`);
+});
